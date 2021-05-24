b724fc5b41b63800fe9f0f8cc27e6cfbcc695d28
MohauMDiscovery <mohau@hooligan.co.za>
2021-05-25 00:08:43 +0200

    refactor WeatherForecastResponse and WeatherResponse

--- WeatherApp/View/WeatherInteractor.swift
+++ WeatherApp/View/WeatherInteractor.swift
@@ -8,6 +8,64 @@
 import Foundation
 import CoreLocation
 
+struct WeatherResponse: Codable {
+    let coord: Coordinate
+    let weather: [Weather]
+    let base: String
+    let main: Main
+    let visibility: Int
+    let wind: Wind
+    let clouds: Cloud
+    let dt: Int
+    let sys: Sys
+    let timezone: Int
+    let id: Int
+    let name: String
+    let cod: Int
+    
+    var viewModel: WeatherViewModel {
+        var weatherType: WeatherType?
+        if let mainWeather = weather.first?.main {
+            weatherType = WeatherType(rawValue: mainWeather.lowercased())
+        }
+        return .init(
+            weatherType: weatherType,
+            city: name,
+            temperature: main.temp,
+            minTemperature: main.temp_min,
+            maxTemperature: main.temp_max
+        )
+    }
+}
+
+struct WeatherForecastResponse: Codable {
+    let cod: String
+    let message: Int
+    let cnt: Int
+    let list: [WeatherForecastItem]
+    let city: City
+    let country: String?
+    let population: Int?
+    let timezone: Int?
+    let sunrise: Int?
+    let sunset: Int?
+    
+    var viewModels: [WeatherForecastItem] {
+        let listGroupedByDay =
+            Dictionary(grouping: list,
+                              by: { Calendar.current.startOfDay(for: Date(timeIntervalSince1970: TimeInterval($0.dt))) })
+                   .compactMap { $0.value.first }
+        
+        let listSortedByDate = listGroupedByDay.sorted {
+            let firstDate = Date(timeIntervalSince1970: TimeInterval($0.dt))
+            let secondDate = Date(timeIntervalSince1970: TimeInterval($1.dt))
+            return firstDate < secondDate
+        }
+        
+        return listSortedByDate
+    }
+}
+
 class WeatherInteractor: NSObject {
     
     // MARK: - View Properties
